9cef05f9c91dee6204b071b2891435e4e318c727
Mahir4098 <mahirjr98@gmail.com>
2026-06-15 14:12:20 +0600

    Solved the 3rd query

--- Query.sql
+++ Query.sql
@@ -85,3 +85,15 @@ from
 where
   full_name ilike 'Tanvir%'
   or full_name ilike '%Haque%'
+
+
+    -- Query 3
+select
+  booking_id,
+  user_id,
+  match_id,
+  coalesce(payment_status, 'Action Required') as systematic_status
+from
+  bookings
+where
+  payment_status is null
